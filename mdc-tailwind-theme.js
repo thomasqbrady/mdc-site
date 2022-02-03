c500f16265c46db7955ce20f4f6785e445294d23
Thomas Q Brady <tqb@thomasqbrady.com>
2022-02-03 14:42:51 -0600

    with values

--- mdc-tailwind-theme.js
+++ mdc-tailwind-theme.js
@@ -1,1 +1,28 @@
-mdc-tailwind-theme.js
+        'mdc': {
+          'primary': '#ff006f',
+          'primary-focus': '#eb0066',
+          'primary-content': '#ffffff',
+
+          'secondary': '#fb5609',
+          'secondary-focus': '#ec4d04',
+          'secondary-content': '#ffffff',
+
+          'accent': '#41d2bd',
+          'accent-focus': '#30cfb7',
+          'accent-content': '#ffffff',
+
+          'neutral': '#242423',
+          'neutral-focus': '#1a1a19',
+          'neutral-content': '#ffffff',
+
+          'base-100': '#8236ec',
+          'base-200': '#7724eb',
+          'base-300': '#6c15e5',
+          'base-content': '#ffffff',
+
+          'info': '#2094f3',
+          'success': '#29bf12',
+          'warning': '#ffbe0a',
+          'error': '#d9263e',
+        },
+ 
